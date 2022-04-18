7c6ac9c93efe6bd002ee33e03e0b762bbb8d3050
devalves2021 <ja13039787@gmail.com>
2022-04-17 21:21:36 -0300

    arquivos maquina virtual

---
+++ index.js
@@ -0,0 +1,16 @@
+import express , {response , resquest} from 'express';
+
+const express = require('express');
+
+const app = express();
+
+app.get('/' , (request, response)=>{
+    return response.json({ message: 'Server is Up!!' });
+})
+
+app.post('/teste', (request,response)=>{
+    const {name, date} = request.body; 
+    return response.json({ name, date });
+})
+
+app.listen(3333);
