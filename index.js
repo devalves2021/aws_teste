89269f7754e31279897c456acc826083cc0ba447
José Alves <90885982+devalves2021@users.noreply.github.com>
2022-04-18 02:29:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express';
 
 const express = require('express');
 
-const app = express();
+const app = express();  
+app.use(express.json());
 
 app.get('/' , (request, response)=>{
     return response.json({ message: 'Server is Up!!' });
